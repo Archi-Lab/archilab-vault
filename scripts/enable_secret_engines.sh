45cf73013dba5cc67a12c14acf7e6ad60badfb2e
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-08 12:18:34 +0100

    Rename K/V engine path to secret

--- scripts/enable_secret_engines.sh
+++ scripts/enable_secret_engines.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-KV_SYS="sys/mounts/kv"
+SECRET_SYS="sys/mounts/secret"
 
 # Enable K/V engine
 curl \
     --location \
     --fail \
     --header "X-Vault-Token: ${VAULT_TOKEN}" \
-    --data "@./data/${KV_SYS}/kv.json" \
-    "${VAULT_ADDR}/v1/${KV_SYS}"
+    --data "@./data/${SECRET_SYS}/secret.json" \
+    "${VAULT_ADDR}/v1/${SECRET_SYS}"
