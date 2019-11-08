63feaf3c2d1e01584d2478104ae4cb4714931fa4
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-08 15:30:19 +0100

    Fix init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ SYS_UNSEAL="sys/unseal"
 # Initialize Vault
 curl \
     --location \
-    --upload-file ./data/${SYS_INIT}/init.json \
+    --upload-file ./data/${SYS_INIT}.json \
     --output "${HOME}/vault-init.json" \
     "${VAULT_ADDR}/v1/${SYS_INIT}"
 
