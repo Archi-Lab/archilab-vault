b061b7996b502692cb7afb4a5aa7379d4ceb8b2b
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-21 10:28:55 +0100

    Fix unseal script

--- scripts/unseal.sh
+++ scripts/unseal.sh
@@ -3,6 +3,7 @@
 set -e
 
 VAULT_DIR="/etc/vault"
+VAULT_ADDR="$(< "${VAULT_DIR}/vault-addr.txt")"
 SYS_UNSEAL="sys/unseal"
 
 jq '. | {key: .keys[0]}' "${VAULT_DIR}/vault-init.json" > "${VAULT_DIR}/vault-unseal.json"
